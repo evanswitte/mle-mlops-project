509efe5c754788ce86a31324cbacc5de1cc37bfa
evanswitte <evo.witte@gmail.com>
2023-07-21 11:35:10 +0200

    train

--- src/train.py
+++ src/train.py
@@ -8,7 +8,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
-from src.preprocessor_nyc_taxi import PreprocessorNycTaxiData
+from preprocessor_nyc_taxi import PreprocessorNycTaxiData
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
